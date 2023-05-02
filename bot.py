a4f1cc4343f0ac1266535a84f23580c6b2c5ffb6
Rafael Ferreira <rafaelpferreira83@gmail.com>
2023-05-02 19:59:48 -0300

    Versao 2 - Processo completo ate protocolar, sem while de mais de uma indisponibilidade

--- bot.py
+++ bot.py
@@ -84,11 +84,14 @@ def main():
     botaoxml = webbot.find_element('button.combo-bot:nth-child(2)',By.CSS_SELECTOR)
     botaoxml.click()
     desktop_bot.wait(1000)
+
     app_path = r"C:\EscribaTeste\Register3\sqlreg.exe"
     desktop_bot.execute(app_path)
-    desktop_bot.connect_to_app(backend=Backend.WIN_32, path=app_path)
+
+    desktop_bot.connect_to_app(backend=Backend.UIA, path=app_path)
 
     desktop_bot.wait(26000)
+
     webbot.stop_browser()
 
     if not desktop_bot.find( "campo_nome", matching=0.97, waiting_time=10000):
@@ -100,17 +103,19 @@ def main():
     desktop_bot.paste('0274')
     desktop_bot.enter()
     desktop_bot.wait(10000)
-
+    #Abrir tela de cadastro de indisponibilidade
     desktop_bot.type_keys(['ctrl', 'f6'])
-    
+    #Acessar botao do TJ
     if not desktop_bot.find( "bt_TJ", matching=0.97, waiting_time=10000):
         not_found("bt_TJ")
     desktop_bot.click()
     desktop_bot.wait(8000)
+    #Carregar arquivo XML
     desktop_bot.type_keys(['alt', 'c'])
     desktop_bot.wait(1000)
     desktop_bot.type_keys(['alt', 'n'])
     desktop_bot.paste(r'C:\RPA\Indisponibilidade\ordens')
+    #Comando para abrir a pasta
     desktop_bot.type_keys(['alt', 'o'])
     desktop_bot.wait(1000)
     desktop_bot.shift_tab()
@@ -122,23 +127,72 @@ def main():
         not_found("atencao")
     desktop_bot.enter()
     desktop_bot.wait(5000)
+    #Processar o arquivo
     desktop_bot.type_keys(['alt', 'p', 'r'])
     desktop_bot.wait(500)
     desktop_bot.type_keys(['alt', 's'])
-
+    #Aguardar o processamento do arquivo
     while desktop_bot.find('aguardar_processamento'):
-        desktop_bot.wait(5000)
-        print(datetime.time)
+        desktop_bot.wait(10000)
+        print(datetime.now())
     else:
+        desktop_bot.wait(2500)
         desktop_bot.type_keys(['alt', 'c'])
+        desktop_bot.wait(1500)
         desktop_bot.type_keys(['alt', 'n'])
         desktop_bot.shift_tab()
         desktop_bot.type_key('o')
         desktop_bot.delete()
-        desktop_bot.key_esc()
-        desktop_bot.type_keys(['alt', 's'])
         desktop_bot.wait(2000)
         desktop_bot.key_esc()
+        desktop_bot.wait(5000)
+
+    while desktop_bot.find('protocolar', waiting_time=5000):
+        desktop_bot.click_at(x=1000, y=425)
+        desktop_bot.wait(1000)
+        desktop_bot.click_at(x=1000, y=425)
+        desktop_bot.control_c()
+        num_comunicado = desktop_bot.get_clipboard()
+        print(num_comunicado)
+        desktop_bot.wait(2000)
+        desktop_bot.right_click_at(x=650, y=425)
+        desktop_bot.wait(2000)
+        #IMPRIMINDO FOLHA DE ROSTO DA INDISPONIBILIDADE
+        desktop_bot.type_key('i')
+        desktop_bot.wait(1000)
+        desktop_bot.tab()
+        desktop_bot.type_key('m')
+        desktop_bot.tab()
+        desktop_bot.wait(500)
+        desktop_bot.type_keys(['alt', 'i'])
+        #SALVANDO O ARQUIVO PDF DA FOLHA DE ROSTO
+        desktop_bot.paste(rf'\\safira\REGISTRO\CNIB\{num_comunicado}')
+        desktop_bot.wait(2000)
+        desktop_bot.enter()
+        desktop_bot.wait(3000)
+        desktop_bot.right_click_at(x=650, y=425)
+        desktop_bot.wait(1000)
+        #PROTOCOLANDO INDISPONIBILIDADE
+        desktop_bot.type_key('p')
+        desktop_bot.wait(3000)
+
+        janela_desconto = desktop_bot.find_app_window(title='Lista de tipos de desconto disponíveis')
+        bt_rolarpaginaabaixo = desktop_bot.find_app_element(from_parent_window=janela_desconto,title='Uma página abaixo')
+        desktop_bot.wait(1000)
+        bt_rolarpaginaabaixo.click()
+        bt_rolarpaginaabaixo.click()
+        bt_rolarpaginaabaixo.click()
+        bt_rolarpaginaabaixo.click()
+        desktop_bot.wait(500)
+        desktop_bot.type_up()
+        desktop_bot.type_keys(['alt', 'l'])
+        desktop_bot.wait(2000)
+    else:
+        desktop_bot.wait(3000)
+        desktop_bot.type_keys(['alt', 's'])
+        desktop_bot.wait(5000)
+        desktop_bot.key_esc()
+        desktop_bot.wait(2000)
         desktop_bot.alt_f4()
 
 
